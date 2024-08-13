7d7814ef04691b421ab564b2382219cee8340c6c
yuhanyuhan <yuhanlian@gmail.com>
2024-08-13 09:48:58 +0800

    added: signals to kill port when server ends

--- main.go
+++ main.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"db/infrastructure"
+	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,4 +21,23 @@ func main() {
 		context.JSON(http.StatusOK, gin.H{"data": "Hello World !"})
 	})
 	router.Run(":8000")
+
+    sigs := make(chan os.Signal, 1)
+
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	done := make(chan bool, 1)
+
+	go func() {
+        sig := <-sigs
+        fmt.Println()
+        fmt.Println(sig)
+        done <- true
+    }()
+
+	fmt.Println("awaiting signal")
+    <-done
+    fmt.Println("exiting")
+
+
 }
